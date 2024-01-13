e2068242a9b853d74ce12e7f2643766292f897bf
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-01-13 12:32:53 +0100

    ciclo while - snack 2

---
+++ Ciclo While - Snack 2/main.js
@@ -0,0 +1,17 @@
+/*
+snack 2
+genera un numero casuale tra 1 e 100 e poi chiedi all'utente di indovinare il numero. Il programma poi deve far comparire un messaggio "il numero è troppo alto" oppure "il numero è troppo basso" nel momento in cui il numero detto dall'utente non è perfettamente uguale al numero estratto in modo casuale
+*/
+
+const randomNumber = Math.floor(Math.random() * 100) + 1
+console.log(randomNumber)
+
+let userNumber = prompt("Inserisci un numero tra 1 e 100 e prova ad indovinare")
+
+if (userNumber > randomNumber) {
+    console.log("Il numero è troppo alto")
+} else if (userNumber < randomNumber) {
+    console.log("Il numero è troppo basso")
+} else {
+    console.log("Bravo, hai indovinato il numero esatto")
+}
