9d04adb78b1b584796517eeddcd5092602a9682e
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-01-13 12:56:41 +0100

    ciclo while - snack 2 - completed

--- Ciclo While - Snack 2/main.js
+++ Ciclo While - Snack 2/main.js
@@ -6,12 +6,26 @@ genera un numero casuale tra 1 e 100 e poi chiedi all'utente di indovinare il nu
 const randomNumber = Math.floor(Math.random() * 100) + 1
 console.log(randomNumber)
 
-let userNumber = prompt("Inserisci un numero tra 1 e 100 e prova ad indovinare")
 
-if (userNumber > randomNumber) {
-    console.log("Il numero è troppo alto")
-} else if (userNumber < randomNumber) {
-    console.log("Il numero è troppo basso")
-} else {
-    console.log("Bravo, hai indovinato il numero esatto")
+let userNumber = 0
+
+while (userNumber !== randomNumber) {
+
+    userNumber = parseInt(prompt("Inserisci un numero tra 1 e 100 e prova ad indovinare"))
+    console.log(userNumber)
+
+    if (userNumber > randomNumber) {
+        console.log("Il numero è troppo alto")
+        alert("Il numero è troppo alto")
+    } else if (userNumber < randomNumber) {
+        console.log("Il numero è troppo basso")
+        alert("Il numero è troppo basso")
+    } else {
+        console.log("Bravo, hai indovinato il numero esatto")
+    }
+
+    userNumber === randomNumber
+
 }
+
+
